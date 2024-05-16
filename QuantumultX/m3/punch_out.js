06699ec5a6c5b12d08d8d49750be5ebc0590d31d
Jun <stupidzhang@aliyun.com>
2024-05-16 14:50:17 +0800

    Update punch_out.js

--- QuantumultX/m3/punch_out.js
+++ QuantumultX/m3/punch_out.js
@@ -1,50 +1,88 @@
-/**
- * @fileoverview Template to compose HTTP reqeuest.
- *
- */
-const crypto = require("crypto");
+const http = require('http');
+const crypto = require('crypto');
 
+const iv = Buffer.from('01234567', 'utf-8');
+const secretKey = Buffer.from('m1yanfa@seeyon.com119$#M1#$', 'utf-8');
 
-const url = 'http://oa.hunktimes.com:9999/mobile_portal/seeyon/rest/attendance/save/m3?option.n_a_s=1';
+function generateKey(secret) {
+    const keyBuffer = Buffer.alloc(24);
+    secret.copy(keyBuffer, 0, 0, Math.min(secret.length, 24));
+    return keyBuffer;
+}
 
-const method = 'POST';
-const deviceId = 'D2528457-4773-4342-928F-C6FBCB0C4029';
-const hostname = 'oa.hunktimes.com:9999';
-const ticket = 'JSESSIONID=78382FE1534299BEBEFB706AE1357286';
-
-
-const myRequest = {
-    url: url,
-    method: method,
-    get headers() {
-        return dynamicHeadersFunction();
-    },
-    get body() {
-        return dynamicBodyFunction();
+function encode(str) {
+    if (!str) return '';
+    try {
+        const cipher = crypto.createCipheriv('des-ede3-cbc', generateKey(secretKey), iv);
+        let encrypted = cipher.update(str, 'utf-8', 'base64');
+        encrypted += cipher.final('base64');
+        return encrypted;
+    } catch (error) {
+        throw new Error(error.message);
     }
 }
 
-function dynamicHeadersFunction() {
-    // 在这里进行逻辑操作，获取需要的头信息
-    return {
-        'accessm3-scheme': 'http',
-        'Connection': 'keep-alive',
-        'Accept-Encoding': 'gzip, deflate',
-        'option.n_a_s': '1',
-        'Content-Type': 'application/json; charset=utf-8',
-        'User-Agent': 'seeyon-m3/4.6.2',
-        'Host': hostname,
-        'cmp-plugins': 'cmp/faceid',
-        'Accept-Language': 'zh_CN',
-        'Accept': 'application/json; charset=utf-8',
-        'Cookie': ticket,
-        'cmp-plugins': 'cmp/faceid',
-        'ltoken': 'DxYTSHQeXs6'
-    };
+
+function delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-function dynamicBodyFunction() {
+const deviceId = 'D2528457-4773-4342-928F-C6FBCB0C4029';
+
+const hostname = 'http://oa.hunktimes.com:9999';
+const cookie = 'JSESSIONID=78382FE1534299BEBEFB706AE1357286';
+
+const commonHeaders = {
+    'accessm3-scheme': 'http',
+    'Connection': 'keep-alive',
+    'Accept-Encoding': 'gzip, deflate',
+    'Content-Type': 'application/json; charset=utf-8',
+    'User-Agent': 'seeyon-m3/4.6.2',
+    'Cookie': cookie,
+    'Host': 'oa.hunktimes.com:9999',
+    'cmp-plugins': 'cmp/faceid',
+    'Accept-Language': 'zh_CN',
+    'Accept': 'application/json; charset=utf-8',
+};
+
+function sign(headers) {
+    return new Promise((resolve, reject) => {
+
+
+        const options = {
+            hostname: hostname,
+            path: 'mobile_portal/seeyon/rest/attendance/save/m3?option.n_a_s=1',
+            method: 'POST',
+            headers: headers,
+        };
+
+        const submitParams = getSubmitParams();
+        const data = JSON.stringify(submitParams);
+
+        const req = http.request(options, (res) => {
+            let responseData = '';
+            res.on('data', (chunk) => {
+                responseData += chunk;
+            });
+            res.on('end', () => {
+                console.info(responseData);
+                resolve();
+            });
+        });
+
+        req.on('error', (error) => {
+            console.error('Error:', error.message);
+            reject(error);
+        });
+
+        req.write(data);
+        req.end();
+    });
+}
+
+function getSubmitParams() {
     const params = {
+
         // 地点
         sign: '杭州网易2（网商路399号）',
         // 类型
@@ -53,9 +91,8 @@ function dynamicBodyFunction() {
         deviceId: deviceId,
         // 1:上班 2:下班
         type: 2,
-        // 经度
+        // 经度维度
         longitude: '120.1977845594618',
-        // 维度
         latitude: '30.18239420572917',
         continent: '',
         // mac地址
@@ -78,15 +115,7 @@ function dynamicBodyFunction() {
         .digest('hex');
     const keySign = crypto
         .createHash('md5')
-        .update(
-            params.sign +
-            params.longitude +
-            params.latitude +
-            params.nearAddress +
-            timestamp +
-            nonce +
-            ''
-        )
+        .update(params.sign + params.longitude + params.latitude + params.nearAddress + timestamp + nonce + '')
         .digest('hex');
     params.timestamp = timestamp + '';
     params.nonce = nonce;
@@ -94,12 +123,16 @@ function dynamicBodyFunction() {
     return params;
 }
 
-$task.fetch(myRequest).then(response => {
-    console.log(response.statusCode + '\n\n' + response.body);
-    $done();
-
-}, reason => {
-    console.log(reason.error);
-    $done();
-
-});
+(async () => {
+    // const randomDelay = Math.floor(Math.random() * (3 * 60 - 1 * 60 + 1) + 1 * 60);
+    const randomDelay = 1;
+    console.log('Waiting for ${randomDelay} seconds before executing...');
+    await delay(randomDelay * 1000);
+    try {
+        if (cookie) {
+            await sign(commonHeaders);
+        }
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+})();
